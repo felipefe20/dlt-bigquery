5672cdf1b0c740724df5005de80d149b2814133f
Oscar Felipe Fernandez <ofelipefernandez@gmail.com>
2024-04-24 18:13:47 -0400

    remove cloud storage

--- dlt_pipeline.py
+++ dlt_pipeline.py
@@ -18,26 +18,6 @@ import os
 #if last_comment_time.tzinfo is None or last_comment_time.tzinfo.utcoffset(last_comment_time) is None:
 #    last_comment_time = pytz.UTC.localize(last_comment_time)
 
-from google.cloud import storage
-
-# Create a storage client
-storage_client = storage.Client()
-
-# Get the bucket and blob
-bucket = storage_client.bucket('youtube_comments_bucket')
-blob = bucket.blob('last_comment.txt')
-
-try:
-    # Download the blob to a string and parse the datetime
-    last_comment_time = datetime.fromisoformat(blob.download_as_text())
-except FileNotFoundError:
-    # If the blob doesn't exist, set the last comment time to the Unix epoch
-    last_comment_time = datetime.fromtimestamp(0, tz=pytz.UTC)
-
-# Make sure last_comment_time is timezone-aware
-if last_comment_time.tzinfo is None or last_comment_time.tzinfo.utcoffset(last_comment_time) is None:
-    last_comment_time = pytz.UTC.localize(last_comment_time)
-#Read the API key from the file secrets.toml
 
 # Load the secrets from the file
 #secrets = toml.load("secrets.toml")
@@ -63,35 +43,35 @@ while True:
 
     if "items" in response:
         for item in response["items"]:
-            comment_time = datetime.fromisoformat(item["snippet"]["topLevelComment"]["snippet"]["publishedAt"].rstrip("Z"))
-            comment_time = comment_time.astimezone(pytz.UTC)  # Make sure the comment time is timezone-aware
+            #comment_time = datetime.fromisoformat(item["snippet"]["topLevelComment"]["snippet"]["publishedAt"].rstrip("Z"))
+            #comment_time = comment_time.astimezone(pytz.UTC)  # Make sure the comment time is timezone-aware
             # If the comment was posted after the last comment time, process it
             
-            if comment_time > last_comment_time:
+            #if comment_time > last_comment_time:
             #pprint(item)
-                published_date=item["snippet"]["topLevelComment"]["snippet"]["publishedAt"]
-                comment_text = item["snippet"]["topLevelComment"]["snippet"]["textDisplay"]
-                author_name = item["snippet"]["topLevelComment"]["snippet"]["authorDisplayName"]
-                like_count=item["snippet"]["topLevelComment"]["snippet"]["likeCount"]
-                total_reply_count=item["snippet"]["totalReplyCount"]
-                video_id=item["snippet"]["videoId"]
-                comments_data.append({"author": author_name, 
-                                "text": comment_text,
-                                "like_count":like_count,
-                                "total_reply_count":total_reply_count,
-                                "video_id":video_id,
-                                    "published_date":published_date
+            published_date=item["snippet"]["topLevelComment"]["snippet"]["publishedAt"]
+            comment_text = item["snippet"]["topLevelComment"]["snippet"]["textDisplay"]
+            author_name = item["snippet"]["topLevelComment"]["snippet"]["authorDisplayName"]
+            like_count=item["snippet"]["topLevelComment"]["snippet"]["likeCount"]
+            total_reply_count=item["snippet"]["totalReplyCount"]
+            video_id=item["snippet"]["videoId"]
+            comments_data.append({"author": author_name, 
+                            "text": comment_text,
+                            "like_count":like_count,
+                            "total_reply_count":total_reply_count,
+                            "video_id":video_id,
+                                "published_date":published_date
                                 })
                 # Update the last comment time
-                last_comment_time = max(last_comment_time, comment_time)
+                #last_comment_time = max(last_comment_time, comment_time)
 
     if "nextPageToken" in response:
         page_token = response["nextPageToken"]
     else:
         break
 # Save the date and time of the last comment processed to a file
-with open("last_comment.txt", "w") as file:
-    file.write(last_comment_time.isoformat())
+#with open("last_comment.txt", "w") as file:
+#    file.write(last_comment_time.isoformat())
 
 
 
@@ -102,7 +82,7 @@ pipeline = dlt.pipeline(pipeline_name="youtube_comments_pipeline",
 # run the pipeline with default settings, and capture the outcome
 info = pipeline.run(comments_data, 
                     table_name="youtube_comments_table", 
-                    write_disposition="append")
+                    write_disposition="replace")
 
 # show the outcome
 print(info)
